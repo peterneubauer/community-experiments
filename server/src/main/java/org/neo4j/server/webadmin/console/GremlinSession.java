be956249207ec09a8defae8df889e32a1cc61051
Peter Neubauer <peter@neubauer.se>
2012-06-09 16:22:59 +0200

    upgrading to Gremlin 2.1.0 bumping to TP 2.1

--- server/src/main/java/org/neo4j/server/webadmin/console/GremlinSession.java
+++ server/src/main/java/org/neo4j/server/webadmin/console/GremlinSession.java
@@ -36,8 +36,8 @@ import org.neo4j.server.database.Database;
 import org.neo4j.server.database.DatabaseBlockedException;
 import org.neo4j.server.logging.Logger;
 
-import com.tinkerpop.blueprints.pgm.TransactionalGraph;
-import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph;
+import com.tinkerpop.blueprints.TransactionalGraph;
+import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
 
 public class GremlinSession implements ScriptSession
 {
